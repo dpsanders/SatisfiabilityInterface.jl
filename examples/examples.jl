867aacfa3474db2062b2f18555cd399296998e35
David P. Sanders <dpsanders@gmail.com>
2021-06-20 12:58:39 -0400

    Some refactoring

--- examples/examples.jl
+++ examples/examples.jl
@@ -155,3 +155,239 @@ constraints = [
 prob = BoundedIntegerCSP(vars, constraints)
 
 solve(prob)
+
+
+### Subset sum Problem
+
+# https://en.wikipedia.org/wiki/Subset_sum_problem
+
+
+# Given a set of integers like {−7, −3, −2, 5, 8}, 
+# do a subset sum to 0?
+
+s = [-7, -3, -2, 5, 8]
+n = length(s)
+x = [Num(Variable(:x, i)) for i in 1:n]
+
+constraints = [
+    # x .∈ Ref(0:1)
+    [x[i] ∈ (0:1) for i in 1:n]
+    sum(x[i] * s[i] for i in 1:n)
+    sum(x) ≠ 0   # at least one is not 0
+    ]
+
+prob = BoundedIntegerCSP(constraints)
+
+all_solutions(prob)
+
+
+
+constraint = x + y <= 1
+lhs, rhs = process(constraint)
+
+
+constraint = x + y <= w + z
+lhs, rhs = process(constraint)
+
+constraint = x + y + w + z == w
+lhs, rhs = process(constraint)
+
+
+
+
+
+constraints = [
+    x ∈ 1:2
+    y ∈ 2:5
+    z ∈ 3:7
+    z == x * y
+    z ≠ 4
+]
+
+prob = ConstraintSatisfactionProblem(constraints)
+prob2 = BoundedIntegerCSP(prob)
+prob3 = encode(prob2)
+
+solve(prob3)
+
+solve(prob2)
+
+prob2.additional_vars[1].domain
+
+
+constraints = [
+    x ∈ 1:2
+    y ∈ 2:5
+    z ∈ 3:7
+    z == x * y
+]
+
+
+prob = ConstraintSatisfactionProblem(constraints)
+prob2 = BoundedIntegerCSP(prob)
+prob3 = encode(prob2)
+prob4 = prob3.p
+
+solve(prob3)
+solve(prob2)
+
+solns = all_solutions(prob4)
+all_solutions(prob3)
+
+### 
+
+constraints = [
+    x ∈ 1:2
+    y ∈ 2:5
+    z ∈ 3:7
+    z == x * y + x
+    x > 1
+]
+
+
+
+prob = BoundedIntegerCSP(constraints)
+all_solutions(prob)
+
+
+
+
+### BeeEncoder example
+
+#=
+@beeint x  0 5
+@beeint y -4 9
+@beeint z -5 10
+
+@constrain x + y == z
+
+@beeint w 0 10
+
+xl = @beebool x[1:4]
+
+@constrain xl[1] == -xl[2]
+@constrain xl[2] == true
+
+@constrain sum([-xl[1], xl[2], -xl[3], xl[4]]) == w
+=#
+
+@variables x, y, z, w
+b = Num.(Variable.(:b, 1:4))
+@variables c, d
+
+constraints = [
+    x ∈ 0:5
+    y ∈ -4:9
+    z ∈ -5:10
+    w ∈ 0:10
+    b .∈ Ref(0:1)  # boolean
+    c ∈ 0:1
+    d ∈ 0:1
+    x + y == z
+    b[1] + b[2] == 1
+    b[2] == 1
+    c + b[1] == 1   # encode c = ¬b[1]
+    d + b[3] == 1
+
+    c + b[2] + d + b[4] == w
+    # sum(b) == w
+
+]
+
+
+prob = ConstraintSatisfactionProblem(constraints)
+prob2 = BoundedIntegerCSP(prob)
+prob3 = encode(prob2)
+
+status, results = solve(prob3)
+
+decode(prob2, results)
+
+solve(prob2)
+
+prob2.original_vars
+
+all_solutions(prob2)
+
+
+
+constraints = [
+    x ∈ 0:5
+    y ∈ -4:9
+    x == 2y
+    # sum(b) == w
+
+]
+
+prob = BoundedIntegerCSP(constraints) 
+solve(prob)
+
+all_solutions(prob)
+
+prob.additional_vars
+
+
+
+constraints = [
+    x ∈ 0:5
+    y ∈ -4:9
+    x == -y
+    # sum(b) == w
+
+]
+
+prob = BoundedIntegerCSP(constraints) 
+solve(prob)
+all_solutions(prob)
+
+
+constraints = [
+    x ∈ 0:5
+    y ∈ -4:9
+    x == -2y
+    # sum(b) == w
+
+]
+
+prob = BoundedIntegerCSP(constraints) 
+all_solutions(prob)
+
+
+constraints = [
+    x ∈ -5:5
+    y ∈ -4:9
+    x == -y^2
+    # sum(b) == w
+
+]
+
+prob = BoundedIntegerCSP(constraints) 
+all_solutions(prob)
+
+
+
+constraints = [
+    x ∈ -5:5
+    y ∈ -4:9
+    x - y^2 == 3
+    # sum(b) == w
+
+]
+
+
+prob = BoundedIntegerCSP(constraints) 
+all_solutions(prob)
+
+
+@variables x, y
+
+constraints = [
+    x ∈ -5:5
+    y ∈ -4:9
+    10x + y == 10
+    # sum(b) == w
+]
+
+prob = BoundedIntegerCSP(constraints) 
+all_solutions(prob)
+
--- src/SatisfiabilityInterface.jl
+++ src/SatisfiabilityInterface.jl
@@ -4,6 +4,7 @@ using Symbolics
 using Symbolics: Variable, Sym
 using Symbolics: Assignment, get_variables, operation, arguments, value, istree
 
+using ReversePropagation
 
 export DiscreteVariable, ConstraintSatisfactionProblem, BoundedIntegerCSP, SymbolicSATProblem, SATProblem
 export read_cnf, SATProblem
@@ -16,12 +17,15 @@ import CommonSolve: solve
 
 include("symbolics_interface.jl")
 include("discrete_variables.jl")
-include("operations.jl")
+
 include("sat_problem.jl")
 include("symbolic_problem.jl")
 include("MOI_wrapper.jl")
 include("read_cnf.jl")
 include("solver.jl")
-include("encode.jl")
+# include("encode.jl")
+
+include("relations.jl")
+
 
 end
